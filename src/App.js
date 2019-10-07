934d9dfac80b940c4d1255ef7769cef45ae555de
PJMantoss <joelptoss@gmail.com>
2019-10-07 19:34:58 +0100

    Convert App to class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <table className="tableHead">
-        <tbody>
-          <tr>
-            <td> <img alt="app icon" width="85" src="https://img.icons8.com/clouds/200/000000/retro-tv.png" /> </td>
-            <td width="10" />
-            <td>
-              <h1>Movies Database</h1>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
 
-      <input placeholder="Search Bar" className="searchBar" />
-
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <table className="tableHead">
+          <tbody>
+            <tr>
+              <td> <img alt="app icon" width="85" src="https://img.icons8.com/clouds/200/000000/retro-tv.png" /> </td>
+              <td width="10" />
+              <td>
+                <h1>Movies Database</h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+  
+        <input placeholder="Search Bar" className="searchBar" />
+  
+      </div>
+    );
+  }
+  
 }
 
 export default App;
