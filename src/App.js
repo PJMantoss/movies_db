e9fbbdabfd72063a1acff11b11bcae24f3d66739
PJMantoss <joelptoss@gmail.com>
2019-10-09 10:05:40 +0100

    Add movie and search components

--- src/App.js
+++ src/App.js
@@ -1,49 +1,21 @@
 import React from 'react';
 import './App.css';
 
+function Movies(){
+  return(
+        <div>
+            <div>{}</div>
+            <div></div>
+        </div>
+  )
+}
+
 class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {}
-    /* this.state = {rows: movieRows}
-    console.log('My Initializer')
-
-    const movies = [
-      {
-        id:0, 
-        poster_src: "https://static.tvmaze.com/uploads/images/medium_portrait/211/529326.jpg", 
-        title:"Raising Dion", 
-        overview:"First overview"
-      },
-      {
-        id:1, 
-        poster_src: "https://static.tvmaze.com/uploads/images/medium_portrait/209/523174.jpg", 
-        title:"Batwoman", 
-        overview:"Second overview"
-      }
-    ]
-
-    const movieRows = []
-    movies.forEach((movie) => {
-       console.log(movie.title)
-       const movieRow = <table key={movie.id} >
-         <tr>
-           <td>
-             <img alt="poster" width="100" src={movie.poster_src} />
-           </td>
-           <td>
-             {movie.title}
-             <p>{movie.overview}</p>
-           </td>
-         </tr>
-       </table>
-       movieRows.push(movieRow)
-    }) */
-
-    this.performSearch = this.performSearch.bind(this)
+  
   }
-
-  performSearch(){}
 
   render(){
     return (
@@ -61,6 +33,10 @@ class App extends React.Component {
         </table>
   
         <input placeholder="Search Bar" className="searchBar" />
+
+        <Movies />
+
+        {this.state.rows}
   
       </div>
     );
